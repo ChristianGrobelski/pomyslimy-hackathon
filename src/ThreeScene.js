450ebdabbea99743b149ae6c019ab22e409c1299
Dariusz Jargus <dariusz.jargus@allegro.com>
2024-05-24 09:54:30 +0200

    duck

--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -1,27 +1,41 @@
 // src/components/ThreeScene.tsx
-import React, { Suspense} from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import React, { Suspense,  useRef, useEffect} from 'react';
+import { Canvas, useLoader, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import {STLLoader} from 'three/examples/jsm/loaders/STLLoader';
-import { Html } from "@react-three/drei"
-const STLModel = ({ modelUrl }) => {
-    const stl = useLoader(STLLoader, modelUrl);
-    return <primitive object={stl} />;
+import duck from './duck.stl';
+
+
+
+export const Model = ({url}) => {
+    const geom = useLoader(STLLoader, url);
+
+    const ref = useRef();
+    const {camera} = useThree();
+    useEffect(() => {
+        camera.lookAt(ref.current.position);
+    });
+
+    return (
+        <>
+            <mesh ref={ref}>
+                <primitive object={geom} attach="geometry"/>
+                <meshStandardMaterial color={"orange"}/>
+            </mesh>
+            <ambientLight/>
+            <pointLight position={[10, 10, 10]}/>
+        </>
+    );
 };
 
 const ThreeScene = () => {
   return (
-    <Canvas
-      camera={{ position: [0, 0, 5] }}
-      style={{ width: '100%', height: '100vh' }}
-    >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Suspense fallback={<Html><span>Loading...</span></Html>}>
-        <STLModel modelUrl="./assets/duck.stl" />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+    <Canvas camera={{ position: [0, 10, 100] }}>
+    <Suspense fallback={null}>
+         <Model url={duck} />
+    </Suspense>
+    <OrbitControls />
+</Canvas>
   );
 };
 
